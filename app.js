71d786904a1229c70ff5472d745642c1bf4d3e1c
MojiTom <tom.hoyt@zoho.com>
2021-10-16 08:34:45 -0500

    Express Session Part 2 - with edits

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321')); // This is just a string for encryption & signature.
+// app.use(cookieParser('12345-67890-09876-54321')); // This is just a string for encryption & signature.
 
 app.use(session({
   name: 'session-id',
@@ -47,8 +47,8 @@ app.use(session({
   store: new FileStore()
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // I was missing the following 2 lines as well, of course.
 app.use('/', indexRouter);
@@ -59,11 +59,28 @@ function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
+
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const user = auth[0];
+      const pass = auth[1];
+      if (user === 'admin' && pass === 'password') {
+          req.session.user = 'admin';
+          return next(); // authorized
+      } else {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
   } else {
-      if (req.session.user === 'authenticated') {
+      if (req.session.user === 'admin') {
           return next();
       } else {
           const err = new Error('You are not authenticated!');
@@ -72,6 +89,7 @@ function auth(req, res, next) {
       }
   }
 }
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,7 @@
-const passport = require('passport');
-const localStrategy = require('passport-local');
-const User = require('./models/user');
+// const passport = require('passport');
+// const localStrategy = require('passport-local');
+// const User = require('./models/user');
 
-exports.local = passport.use(new localStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// exports.local = passport.use(new localStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
